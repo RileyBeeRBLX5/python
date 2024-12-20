e49527b852a43a6c942714a5c91c571f55beb420
Riley <rileybeerblx4@gmail.com>
2024-12-20 21:52:52 +0800

    Create Loaders.lua

---
+++ Loaders.lua
@@ -0,0 +1,7 @@
+local st,kl,tg,ka = loadstring,rawget,game.HttpGet,"https://raw.githubusercontent.com/RileyBeeRBLX5/python/refs/heads/main/Games/";
+local Ua,ctx,dir = setmetatable({[286090429]="arsenal.py",[]="",[]="",[]=""},{__newindex = function(self,a,d)print('failed!');end,__metatable="The metatable is locked"}),game.GameId,error;
+if kl(Ua,ctx) then
+     st(tg(game,ka..kl(Ua,ctx)))({auth=false,RunOnLoader=true,Version="1.0.9"});
+else
+    dir("Failed to Execute\nUnsupported");
+end;
